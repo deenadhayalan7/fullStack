4c6d342f4055e3106f8bd5e77dbdba67eeab0912
deenadhayalan7 <113279582+deenadhayalan7@users.noreply.github.com>
2024-01-31 15:21:15 +0530

    Add files via upload

---
+++ Day-5/src/components/Mainpage/shoppingData.js
@@ -0,0 +1,21 @@
+const shoppingData = [
+  {
+    id: 1,
+    title: "50% Off On Your First Shopping",
+    desc: "Top selling collections in gifter",
+    cover: "./assets/main-slider/slides-4.png",
+  },
+  {
+    id: 2,
+    title: "50% Off On Your First Shopping",
+    desc: "Great deals in jewels",
+    cover: "./assets/main-slider/slides-2.png",
+  },
+  {
+    id: 3,
+    title: "50% Off On Your First Shopping",
+    desc: "best collections in fashion with a huge discounts",
+    cover: "./assets/main-slider/slides-5.png",
+  },
+];
+export default shoppingData;
