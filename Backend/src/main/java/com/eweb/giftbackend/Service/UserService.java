0eedd895e356ca71a8d521692f408e5c25bade15
deenadhayalan7 <113279582+deenadhayalan7@users.noreply.github.com>
2024-02-14 16:21:40 +0530

    Delete Backend directory

--- Backend/src/main/java/com/eweb/giftbackend/Service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.eweb.giftbackend.Service;
-
-import com.eweb.giftbackend.dto.response.BasicResponse;
-import com.eweb.giftbackend.dto.response.UserResponse;
-
-public interface UserService {
-    BasicResponse<UserResponse>getAllUser();
-}
--- Backend/src/main/java/com/eweb/giftbackend/enumerated/Role.java
+++
@@ -1,5 +0,0 @@
-package com.eweb.giftbackend.enumerated;
-
-public enum Role {
-    USER,ADMIN
-}
